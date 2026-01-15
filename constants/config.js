0dbebcc42efc57703709a4295027a365201a7e9f
Parth singh <ps.kuntal2005@gmail.com>
2026-01-15 23:46:04 +0530

    Remove API keys from config.js
    Removed sensitive API keys from config.js.

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,5 @@
-export const ORS_API_KEY = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImNiZWQzZGViZGVhOTQ3NTRiMmU3ZTVmMGQ5YmFkMmIzIiwiaCI6Im11cm11cjY0In0=";
-export const OWM_API_KEY = "268229c579a16948745591681f518d8a";
+export const ORS_API_KEY = 
+export const OWM_API_KEY = 
 
 export const DEFAULT_ZOOM = {
   latitudeDelta: 0.05,
